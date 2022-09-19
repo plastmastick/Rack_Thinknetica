a01111095f97d382b482fefa0151d2bb3a136579
plastmastick <nick.razum23@gmail.com>
2022-09-19 20:25:24 +0300

    feat: create simple rack

---
+++ app.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rack'
+
+class App
+
+  def call(env)
+    [status, headers, body]
+  end
+
+  private
+
+  def status
+    200
+  end
+
+  def headers
+    { 'Content-Type' => 'text/plain' }
+  end
+
+  def body
+    ["Welcome aboard!\n"]
+  end
+
+end
